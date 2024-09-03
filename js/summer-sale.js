f03e8d8b347d82a76774499d205183955599b7e8
sultan-cse <nishtan49@gmail.com>
2024-09-04 04:28:14 +0600

    cupon-text-fixed-remaining

--- js/summer-sale.js
+++ js/summer-sale.js
@@ -1,40 +1,57 @@
-function handClick(data)
-{
-    const title = data.childNodes[5].innerText
-    const price = parseFloat(data.childNodes[7].innerText.split(" ")[0]);
-    const titleContainer = document.getElementById('title-container');
-    const count = titleContainer.childElementCount;
-    const h5 = document.createElement('h5');
-    h5.innerHTML = `
-    ${count+1}. ${title}
-    `
-    h5.setAttribute('class','text-xl font-medium');
-    titleContainer.appendChild(h5)
-    const PriceContainer = document.getElementById('price-container');
-    const previousPrice = parseFloat(PriceContainer.children[0].children[0].innerText.split(" ")[0])
-    total = previousPrice + price;
-    PriceContainer.children[0].children[0].innerText = total.toFixed(2) + " TK";
-    if(total>0){
-        const modalBtn = document.getElementById('modal-btn');
-        modalBtn.removeAttribute('disabled',true)
-        
-    }
-    if(total>=200){
-        const applyBtn = document.getElementById('apply-btn');
-        applyBtn.removeAttribute('disabled',true)
-        cuponText = document.getElementById('cupon-text')
-        if(cuponText.value === "SELL200"){
-            apply();
-        }
+let globalTotal = 0;
 
-    }
-    
-    
+function handClick(data) {
+  let title = data.childNodes[5].innerText;
+  let price = parseFloat(data.childNodes[7].innerText.split(" ")[0]);
+  let titleContainer = document.getElementById("title-container");
+  let count = titleContainer.childElementCount;
+  let h5 = document.createElement("h5");
+  h5.innerHTML = `${count + 1}. ${title}`;
+  h5.setAttribute("class", "text-xl font-medium");
+  titleContainer.appendChild(h5);
 
+  let PriceContainer = document.getElementById("price-container");
+  let previousPrice = parseFloat(
+    PriceContainer.children[0].children[0].innerText.split(" ")[0]
+  );
+
+  globalTotal = previousPrice + price;
+
+  PriceContainer.children[0].children[0].innerText =
+    globalTotal.toFixed(2) + " TK";
+
+  if (globalTotal > 0) {
+    let modalBtn = document.getElementById("modal-btn");
+    modalBtn.removeAttribute("disabled");
+  }
+
+  if (globalTotal >= 200) {
+    let applyBtn = document.getElementById("apply-btn");
+    applyBtn.removeAttribute("disabled");
+  }
 }
 
 function apply() {
-    const PriceContainer = document.getElementById('price-container');
-    PriceContainer.children[1].children[0].innerText = (total*0.2).toFixed(2) + " Tk";
-    PriceContainer.children[2].children[0].innerText = (total-(total*0.2)).toFixed(2) + " Tk";
+  let cuponText = document.getElementById("cupon-text").value;
+  document.getElementById("cupon-text").value = "";
+  let sellBtn = document.getElementById("sell-btn").innerText;
+  let PriceContainer = document.getElementById("price-container");
+
+  if (globalTotal >= 200 && cuponText === sellBtn) {
+    let discount = globalTotal * 0.2;
+    let finalTotal = globalTotal - discount;
+
+    PriceContainer.children[1].children[0].innerText =
+      discount.toFixed(2) + " TK";
+    PriceContainer.children[2].children[0].innerText =
+      finalTotal.toFixed(2) + " TK";
+  }
+
+  else{
+    
+    PriceContainer.children[1].children[0].innerText ="00 TK";
+    PriceContainer.children[2].children[0].innerText = "00 TK";
+  }
+  
+  
 }
