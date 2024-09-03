6b2514c8115a007614c5f622fb6cdebc7cee18a7
sultan-cse <nishtan49@gmail.com>
2024-09-04 01:40:03 +0600

    modaldone

--- js/summer-sale.js
+++ js/summer-sale.js
@@ -10,11 +10,21 @@ function handClick(data)
     h5.innerHTML = `
     ${count+1}. ${title}
     `
-    h5.setAttribute('class','text-xl xl:text-2xl font-medium');
+    h5.setAttribute('class','text-xl font-medium');
     titleContainer.appendChild(h5)
     const PriceContainer = document.getElementById('price-container');
     const previousPrice = parseFloat(PriceContainer.children[0].children[0].innerText.split(" ")[0])
     console.log(previousPrice)
+    total = previousPrice + price;
+    PriceContainer.children[0].children[0].innerText = total.toFixed(2) + "TK";
+    if(total>0){
+        const modalBtn = document.getElementById('modal-btn');
+        modalBtn.removeAttribute('disabled',true)
+    }
+    if(total>=200){
+        const applyBtn = document.getElementById('apply-btn');
+        applyBtn.removeAttribute('disabled',true)
+    }
     
     
 
